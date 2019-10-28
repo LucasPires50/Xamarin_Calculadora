d8960eebc154d56cf6b57927e1f909e4968e8fed
Lucas Pires <luc_50@outlook.com>
2019-10-28 17:35:46 -0300

    Impementado as fonções

--- Xamarin_Calculadora/Xamarin_Calculadora/MainPage.xaml.cs
+++ Xamarin_Calculadora/Xamarin_Calculadora/MainPage.xaml.cs
@@ -13,9 +13,36 @@ namespace Xamarin_Calculadora
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        int currentSatate = 1;
+        String Operção;
+        double primeiro_numero, segundo_numero;
+
         public MainPage()
         {
             InitializeComponent();
+            btnLimpar_Clicked(new object(), new EventArgs());
+        }
+
+        private void btnLimpar_Clicked(object sender, EventArgs e)
+        {
+            primeiro_numero = 0;
+            segundo_numero = 0;
+            currentSatate = 1;
+            this.txtResultado.Text = "0";
+        }
+
+        void selecionar_numero(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string pressionar = button.Text;
+
+            if (this.txtResultado.Text == "0" || currentSatate < 0)
+            {
+                this.txtResultado.Text = "";
+                if (currentSatate < 0)
+                    currentSatate *= -1;
+
+            }
         }
 
         private void Button_Clicked(object sender, EventArgs e)
